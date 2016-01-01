b559653f6a637ce000b9141f4d4eab0af2880eed
David Bau <david.bau@gmail.com>
2016-01-01 10:11:45 -0500

    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(
+    name='Python Templet',
+    version='4.0.0',
+    description='Lightweight string templating via function decorator.',
+    long_description=open('README.md', 'r').read(),
+    author='David Bau',
+    author_email='david.bau@gmail.com',
+    url='https://github.com/python-templet/templet',
+    packages=[],
+    install_requires=[],
+    dependency_links=[],
+    scripts=[],
+    license='BSD',
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Text Processing :: General",
+    ]
+)
